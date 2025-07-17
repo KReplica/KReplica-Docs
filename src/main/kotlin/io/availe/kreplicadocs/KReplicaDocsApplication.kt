a6c2510d10cb6643bdfc958f3cec4f1f346d8faf
availe <94802525+availe@users.noreply.github.com>
2025-07-17 19:07:09 -0400

    project init

---
+++ src/main/kotlin/io/availe/kreplicadocs/KReplicaDocsApplication.kt
@@ -0,0 +1,11 @@
+package io.availe.kreplicadocs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KReplicaDocsApplication
+
+fun main(args: Array<String>) {
+	runApplication<KReplicaDocsApplication>(*args)
+}
---
+++ src/test/kotlin/io/availe/kreplicadocs/KReplicaDocsApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.availe.kreplicadocs
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KReplicaDocsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
