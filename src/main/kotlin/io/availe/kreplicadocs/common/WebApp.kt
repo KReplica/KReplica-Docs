4b1cda00560c71631e257da03a7432ee33d184d5
availe <94802525+availe@users.noreply.github.com>
2025-08-01 18:51:12 -0400

    remove objects

--- src/main/kotlin/io/availe/kreplicadocs/common/WebApp.kt
+++ src/main/kotlin/io/availe/kreplicadocs/common/WebApp.kt
@@ -19,31 +19,9 @@ object WebApp {
             const val COMPILE = "/playground/compile"
         }
 
-        object Examples {
-            private const val BASE = "/examples/{slug}"
-            const val PLAYGROUND_FILE_SWAP = "$BASE/playground-file-swap/{fileName}"
-            const val GENERATED_PANEL = "$BASE/generated-panel/{fileName}"
-            const val FILE_CONTENT = "$BASE/file/{fileName}"
-            const val FILE_CONTENT_ONLY = "$BASE/file-content/{fileName}"
-        }
-
         object Api {
             const val COMPLETIONS = "/api/completions"
         }
     }
 
-    object ViewModelAttributes {
-        const val NAV_LINKS = "navLinks"
-        const val CURRENT_PAGE = "currentPage"
-        const val ALL_EXAMPLES = "allExamples"
-        const val EXAMPLE = "example"
-        const val FEATURE_EXAMPLE = "featureExample"
-        const val ACTIVE_SLUG = "activeSlug"
-        const val ACTIVE_FILE = "activeFile"
-        const val LANGUAGE = "language"
-        const val CODE = "code"
-        const val PROPERTIES = "properties"
-        const val TOUR_SELECT_OPTIONS = "tourSelectOptions"
-        const val EXAMPLE_SELECT_OPTIONS = "exampleSelectOptions"
-    }
 }
