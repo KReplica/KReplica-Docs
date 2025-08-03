5acefd03bdc22b4189bbe9af779692b6af2194ca
availe <94802525+availe@users.noreply.github.com>
2025-08-03 15:13:36 -0400

    Update advanced-serialization.kt

--- src/main/resources/playground-templates/advanced-serialization.kt
+++ src/main/resources/playground-templates/advanced-serialization.kt
@@ -13,6 +13,7 @@ import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import java.time.Instant
 
+// For the purpose of this example, we're creating a custom KSerializer.
 object InstantAsStringSerializer : KSerializer<Instant> {
     override val descriptor: SerialDescriptor =
         PrimitiveSerialDescriptor("Instant", PrimitiveKind.STRING)
@@ -28,17 +29,23 @@ object InstantAsStringSerializer : KSerializer<Instant> {
 
 private interface UserAccount
 
+// KReplica has automatic @Contextual generation by default, but for
+// this example, we disabled it via "autoContextual = AutoContextual.DISABLED"
 @Replicate.Model(variants = [DtoVariant.DATA], autoContextual = AutoContextual.DISABLED)
 @Replicate.Apply([Serializable::class])
 private interface V1 : UserAccount {
     val id: Int
 
+    // Since auto-contextual is off, we must manually apply @Contextual for this complex type.
     @Contextual
     val startTime: Instant
 
+    // Alternatively, you can explicitly provide a custom serializer.
     @Serializable(with = InstantAsStringSerializer::class)
     val midTime: Instant
 
+    // Or, you can override the model-level setting and re-enable automatic @Contextual
+    // generation for just this property. KReplica will handle it from here.
     @Replicate.Property(autoContextual = AutoContextual.ENABLED)
     val endTime: List<List<Instant>>
 }
