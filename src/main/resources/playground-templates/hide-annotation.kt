0c47a32cdf96c9eef61a38f4560ab93ddfa552ac
availe <94802525+availe@users.noreply.github.com>
2025-08-03 16:04:09 -0400

    hide annotation comments

--- src/main/resources/playground-templates/hide-annotation.kt
+++ src/main/resources/playground-templates/hide-annotation.kt
@@ -3,13 +3,10 @@ package io.availe.demo.playground
 import io.availe.Replicate
 import io.availe.models.DtoVariant
 
+// The @Replicate.Hide annotation tells KReplica to skip code generation for this model.
+// This is useful for temporarily testing the impact of removing a schema.
 @Replicate.Model(variants = [DtoVariant.DATA])
 @Replicate.Hide
 private interface HiddenAccount {
     val id: Int
-}
-
-@Replicate.Model(variants = [DtoVariant.DATA])
-private interface VisibleAccount {
-    val name: String
 }
