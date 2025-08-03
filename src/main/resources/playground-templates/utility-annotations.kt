5ad0b125edba762b531bb9bba9ab93fc132b1f15
availe <94802525+availe@users.noreply.github.com>
2025-08-03 16:01:00 -0400

    Update utility-annotations.kt

--- src/main/resources/playground-templates/utility-annotations.kt
+++ src/main/resources/playground-templates/utility-annotations.kt
@@ -3,6 +3,9 @@ package io.availe.demo.playground
 import io.availe.Replicate
 import io.availe.models.DtoVariant
 
+// --- Direct Annotation Application ---
+// Standard annotations like @Deprecated can be applied directly to the interface.
+// KReplica will copy them to the generated DTOs.
 @Replicate.Model(variants = [DtoVariant.DATA])
 @Deprecated("Use NewUserAccount instead")
 private interface LegacyAccount {
@@ -10,14 +13,20 @@ private interface LegacyAccount {
     val id: Int
 }
 
+// --- Hiding a Model ---
+// The @Replicate.Hide annotation tells KReplica to skip code generation for this model.
+// This is useful for temporarily testing the impact of removing a schema.
 @Replicate.Model(variants = [DtoVariant.DATA])
 @Replicate.Hide
 private interface HiddenAccount {
     val id: Int
 }
 
+// --- Manually Specify Version Number ---
+// If you do not wish to use the V<number> naming convention for versions...
 private interface Product
 
+// ...you can use @Replicate.SchemaVersion to manually specify the version number.
 @Replicate.Model(variants = [DtoVariant.DATA])
 @Replicate.SchemaVersion(1)
 private interface InitialProduct : Product {
