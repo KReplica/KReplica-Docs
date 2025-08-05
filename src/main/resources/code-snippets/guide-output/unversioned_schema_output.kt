6ad5ebf3352defd6ba94fa97c3f5bffdea6f6ca8
availe <94802525+availe@users.noreply.github.com>
2025-08-05 17:31:23 -0400

    Update unversioned_schema_output.kt

--- src/main/resources/code-snippets/guide-output/unversioned_schema_output.kt
+++ src/main/resources/code-snippets/guide-output/unversioned_schema_output.kt
@@ -1,15 +1,5 @@
 public sealed interface <SchemaName>Schema {
-    public data class Data(
-        val id: <IdType>,
-        val name: String
-    ) : <SchemaName>Schema, KReplicaDataVariant<<SchemaName>Schema>
-
-    public data class CreateRequest(
-        val name: String
-    ) : <SchemaName>Schema, KReplicaCreateVariant<<SchemaName>Schema>
-
-    public data class PatchRequest(
-        val id: Patchable<<IdType>> = Patchable.Unchanged,
-    val name: Patchable<String> = Patchable.Unchanged
-    ) : <SchemaName>Schema, KReplicaPatchVariant<<SchemaName>Schema>
+    public data class Data(...) : <SchemaName>Schema, ...
+    public data class CreateRequest(...) : <SchemaName>Schema, ...
+    public data class PatchRequest(...) : <SchemaName>Schema, ...
 }
