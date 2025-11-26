e6112758c1d0d15d34c2b4d4a78ccbd3cdc9ffef
availe <94802525+availe@users.noreply.github.com>
2025-11-25 21:02:21 -0500

    chore(deps): update kreplica and ksp

--- src/main/resources/code-snippets/general/quickstart-build.kts
+++ src/main/resources/code-snippets/general/quickstart-build.kts
@@ -1,5 +1,5 @@
 plugins {
     // Use a KSP version compatible with your Kotlin version
-    id("com.google.devtools.ksp") version "2.2.20-2.0.3"
-    id("io.availe.kreplica") version "5.0.4"
+    id("com.google.devtools.ksp") version "2.3.3"
+    id("io.availe.kreplica") version "5.3.0"
 }
